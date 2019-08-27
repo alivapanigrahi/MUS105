2e7d95fe4c67116825d8f2954893eb5470f6d57a
Aliva <aliva_panigrahi@yahoo.com>
2019-08-27 13:38:51 -0500

    adding builtins doc

---
+++ Builtins.py
@@ -0,0 +1,43 @@
+# math builtin functions
+
+abs()
+min()
+divmod()
+max()
+round()
+pow()
+
+# constructor builtin functions
+
+set()
+dict()
+bool()
+complex()
+bytes()
+float()
+tuple()
+int()
+str()
+bytearray()
+frozenset()
+list()
+
+# introspection builtin functions
+
+id()
+isinstance()
+issubclass()
+dir()
+type()
+locals()
+vars()
+globals()
+callable()
+hash()
+memoryview()
+help()
+
+# classes builtin functions
+
+
+
