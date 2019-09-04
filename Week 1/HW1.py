68a7adec51abd4cc5e15504b9313a0922c0ce7f9
Aliva <aliva_panigrahi@yahoo.com>
2019-09-04 13:30:15 -0500

    more week 1 hw edits

--- Week 1/HW1.py
+++ Week 1/HW1.py
@@ -10,3 +10,15 @@ print(power(2, 3))
 def list_sum(l):
   return sum(l)
 print(list_sum([1,2.0,3,4.0]))
+
+# remove substring function
+def remove_substring_instances(in_str, substr):
+  new_string = in_str.replace(in_str, substr)
+  return(new_string, in_str.count(substr))
+print(remove_substring_instances("hello", "ell"))
+
+# list to string function
+def list_to_str(l):
+  for i in l:
+    return(str(i))
+print(list_to_str([1, 2, 3, 4, 5]))
