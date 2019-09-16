2cfb5af3d9f52faab0adf823a18b793986795fcf
Aliva <aliva_panigrahi@yahoo.com>
2019-09-16 16:14:43 -0500

    updating hw

--- Week 2/hw1.py
+++ Week 2/hw1.py
@@ -24,49 +24,28 @@
 #############################################################################################################
 
 def is_palindrome(s):
-    """
-    given a string s, output whether or not s is a palindrome. A palindrome is defined as a string that is identical
-    read forwards or backwards. for example:
-        'racecar' is a palindrome.
-        'race car' is NOT a palindrome
-        'rac e car' is a palindrome
-    *for out purposes, spaces matter
+    reverse = s[::-1]
+    if s == reverse:
+        return True
+    else:
+        return False
 
-    :param s: string to test for a palindrome
-    :type s: str
-    :return: whether or not s is a palindrome
-    :rtype: bool
-    """
     pass
 
 def is_in(a, b):
-    
-    
-    """
-    given a sequence of characters (string) b, output whether or not string a can be constructed with the letters of
-    string b. For example:
-        is_in('cab', 'aabbcc') should output true
-        is_in('hello world', 'helo world') should output false
-
-    :param a: string to try and construct
-    :type a: str
-    :param b: string to clip characters from
-    :type b: str
-    :return: whether or not a can be constructed from b
-    :rtype: bool
-    """
-    
+    list(a)
+    for i in a:
+        if i in b:
+            return True
+        else:
+            return False
 
 def is_set(l):
-    """
-    given a list of integers l, output whether or not l is a set. Recall that a set is a collection of UNIQUE items.
-    the type of l will be list. When we say 'is l a set?' we mean in the mathematical sense, not the data type sense!
+    if(len(set(l)) == len((l))):
+        return True
+    else:
+        return False
 
-    :param l: list to check uniqueness on
-    :type l: list
-    :return: whether or not l is a set
-    :rtype: bool
-    """
     pass
 
 def str_to_int2(num_string):
@@ -138,19 +117,6 @@ class Course:
         print("The University is: " + Course.university)
         
     def __init__(self, code, crn, description):
-        """
-        Initializer, your code should set instance variables with the same name as the parameters to their respective
-        parameter. The first one is done for you.
-
-        **It should also initialize one more instance variable, roster, to an empty list**
-
-        :param code: course code
-        :type code: str
-        :param crn: course CRN
-        :type crn: int
-        :param description: course description
-        :type description: str
-        """
 
         self.code = code
         self.crn = crn
@@ -163,6 +129,7 @@ class Course:
         pass
 
     def remove_student(self, student):
+        
         """
         removes a student from the roster, by finding their index, and removing them from the roster list.
         if the function is called on a student who is not enrolled in a course (i.e. they cannot be found in the roster)
@@ -175,9 +142,11 @@ class Course:
 
     def get_description(self):
         return self.description
-
+'''
 Course.print_school()
-
 mus105 = Course("MUS105", 71866, "Computation & Music 1")
 mus105.add_student("alivabp2")
 print(mus105.get_description())
+is_palindrome("race car")
+
+'''
